71cad8a934f05bad575906ea4c85c890cf49b81d
OmarMahmoud2002 <omarmahmoud.devv@gmail.com>
2025-06-02 14:11:32 +0300

    Adding HMS module files

---
+++ temp_hms/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name": "Hospital Management System",
+    "summary": "Manage hospital patients and records",
+    "description": """
+        Hospital Management System module for managing:
+        - Patients
+        - Patient Records
+        - Medical History
+        - Departments
+        - Doctors
+    """,
+    "author": "Omar Mahmoud",
+    "category": "Health",
+    "version": "1.0",
+    "depends": ["base", "contacts"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hms_menus.xml",
+        "views/patient_views.xml",
+        "views/department_views.xml",
+        "views/doctors_views.xml",
+        "views/patient_log_views.xml",
+        "views/res_partner_views.xml",
+    ],
+    "application": True,
+    "installable": True,
+}
